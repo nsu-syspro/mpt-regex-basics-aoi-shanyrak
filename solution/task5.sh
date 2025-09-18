7c2fa6edf5c5152e26e8f24c5464fbc8fd6b2282
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-18 08:10:10 +0700

    one regex

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sed -E 's/^([^,]+),([^,]+)$/\1 (\2)/' data/students.csv |
-sed -E 's/^([^ ]+) ([^ ]+)/\2 \1/' |
-sed -E 's/^([^ ]+) ([^ ]+)( [^ ]+)? (\([0-9]+\))$/\1 \2 \4/' > data/students.txt
+sed -E 's/^([^ ]+) ([^ ]+)( [^ ]+)?,([0-9]+)$/\2 \1 (\4)/' data/students.csv > data/students.txt
